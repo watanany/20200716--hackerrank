4e4da7f5571490f62165055dd1b7297b1c4fbfb4
渡部真伍 <s1170087@gmail.com>
2019-08-01 00:29:36 +0900

    change:

--- a-very-big-sum/hs.hs
+++ a-very-big-sum/hs.hs
@@ -2,12 +2,23 @@
 
 module Main where
 
+{-
 main :: IO ()
 main = do
   _ <- readLn :: IO Integer
   line <- getLine
   let ar = map read $ words line :: [Integer]
   print $ aVeryBigSum ar
+-}
+
+
+-- version not using do expression
+main :: IO ()
+main =
+  (readLn :: IO Integer) >>
+    getLine >>=
+      \line ->
+        (\ar -> print $ aVeryBigSum ar) $ (map read $ words line :: [Integer])
 
 
 -- version not using sum
